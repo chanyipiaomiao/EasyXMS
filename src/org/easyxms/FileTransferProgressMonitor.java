5ea54348317d7beb2a557486a6949e2bb8676317
chanyipiaomiao <chanyipiaomiao@163.com>
2014-08-29 14:42:12 +0800

    修改一些文件

--- src/org/easyxms/FileTransferProgressMonitor.java
+++ src/org/easyxms/FileTransferProgressMonitor.java
@@ -1,34 +1,34 @@
 package org.easyxms;
 
+
 import com.jcraft.jsch.SftpProgressMonitor;
 import java.text.DecimalFormat;
 
-class FileUploadProgressMonitor implements SftpProgressMonitor {
+
+class FileTransferProgressMonitor implements SftpProgressMonitor{
 
     private long transfered;
     private static long filesize;
 
 
     @Override
-    public void init(int op, String src, String dst, long file_length) {
-        System.out.println("The Transferring Start ...");
-    }
+    public void init(int op, String src, String dst, long file_length) {}
 
     @Override
     public boolean count(long count) {
         transfered = transfered + count;
         DecimalFormat percent_format = new DecimalFormat("#.##");
         double percent = ((double)transfered * 100)/(double)filesize;
-        System.out.printf("\rProgress: %s%% Transfered: %d Total: %d",percent_format.format(percent),transfered,filesize);
+        System.out.printf("\r%s%% Transfered: %d Total: %d",percent_format.format(percent),transfered,filesize);
         return true;
     }
 
     @Override
     public void end() {
-        System.out.print("\nTransfered Complete");
+        System.out.print("\nTransfer Completed.");
     }
 
     public static void setFilesize(long filesize) {
-        FileUploadProgressMonitor.filesize = filesize;
+        FileTransferProgressMonitor.filesize = filesize;
     }
 }
---
+++ src/org/easyxms/TransferFile.java
@@ -0,0 +1,6 @@
+package org.easyxms;
+
+
+public interface TransferFile {
+
+}
