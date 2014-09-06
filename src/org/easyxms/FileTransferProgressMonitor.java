076eaa035c20ae04f4c1181d77caa47d751e9b67
chanyipiaomiao@163.com <quiou130>
2014-09-06 21:15:09 +0800

    1.修改一些文件

--- src/org/easyxms/FileTransferProgressMonitor.java
+++ src/org/easyxms/FileTransferProgressMonitor.java
@@ -11,28 +11,32 @@ class FileTransferProgressMonitor implements SftpProgressMonitor{
     private static long filesize;
     private String ip;
 
+
     FileTransferProgressMonitor(String ip) {
         this.ip = ip;
     }
 
+
     @Override
-    public void init(int op, String src, String dst, long file_length) {
-    }
+    public void init(int op, String src, String dst, long file_length) {}
+
 
     @Override
     public boolean count(long count) {
         transfered = transfered + count;
         DecimalFormat percent_format = new DecimalFormat("#.##");
         double percent = ((double)transfered * 100)/(double)filesize;
-        System.out.printf("\r%s %s %s%% Transfered: %d Total: %d", ip, "Transferring...",
+        System.out.printf("\r%s%s %s%% Transfered: %d Total: %d",ip,"Transferring...",
                 percent_format.format(percent), transfered, filesize);
         return true;
     }
+
 
     @Override
     public void end() {
         System.out.println();
     }
+
 
     public static void setFilesize(long filesize) {
         FileTransferProgressMonitor.filesize = filesize;
