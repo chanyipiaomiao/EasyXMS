52150260b3333881492a719e0582608de9e63e90
chanyipiaomiao <chanyipiaomiao@163.com>
2014-10-11 16:27:53 +0800

    修改一些文件

--- src/org/easyxms/EasyXMS.java
+++ src/org/easyxms/EasyXMS.java
@@ -5,13 +5,13 @@ public class EasyXMS {
 
     public static void main(String[] args){
 
-        CommandLineOptionsParser.optionArgsParser(args);
+//        CommandLineOptionsParser.optionArgsParser(args);
 
-//        //读取配置文件并设置
-//        SettingInfo.settingInfo();
-//
-//        //开始执行
-//        ChoiceNumber choiceNumber = new ChoiceNumber();
-//        choiceNumber.loopGetValue();
+        //读取配置文件并设置
+        SettingInfo.settingInfo();
+
+        //开始执行
+        ChoiceNumber choiceNumber = new ChoiceNumber();
+        choiceNumber.loopGetValue();
     }
 }
